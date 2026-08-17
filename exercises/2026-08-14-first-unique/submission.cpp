726ebd2d0d0c936a3b7b6eef400f19a8321524ab
SEU-ZiyuHuang <213220760@seu.edu.cn>
2026-08-17 16:02:24 +0800

    Refactor First Unique index types and no-result handling

--- exercises/2026-08-14-first-unique/submission.cpp
+++ exercises/2026-08-14-first-unique/submission.cpp
@@ -2,53 +2,61 @@
 #include<vector>
 #include<unordered_map>
 
-int firstUniqueIndex(const std::vector<int>& nums);
-void printResult(const std::vector<int>& nums, int result);
+std::vector<int>::size_type firstUniqueIndex(const std::vector<int>& nums);
+void printResult(const std::vector<int>& nums, std::vector<int>::size_type result);
 
 int main()
 {
     std::vector<int> nums1 = {4, 5, 1, 2, 0, 4}, nums2 = {1, 1, 2, 2}, nums3 = {7}, nums4 = {2, 3, 2, 4, 3, 5};
-    int result1 = firstUniqueIndex(nums1);
-    int result2 = firstUniqueIndex(nums2);
-    int result3 = firstUniqueIndex(nums3);
-    int result4 = firstUniqueIndex(nums4);
+    std::vector<int>::size_type result1 = firstUniqueIndex(nums1);
+    std::vector<int>::size_type result2 = firstUniqueIndex(nums2);
+    std::vector<int>::size_type result3 = firstUniqueIndex(nums3);
+    std::vector<int>::size_type result4 = firstUniqueIndex(nums4);
     printResult(nums1, result1);
     printResult(nums2, result2);
     printResult(nums3, result3);
     printResult(nums4, result4);
 }
 
-int firstUniqueIndex(const std::vector<int>& nums)
+std::vector<int>::size_type firstUniqueIndex(const std::vector<int>& nums)
 {
     std::unordered_map<int, int> count;
-    for(int i = 0; i < nums.size(); i++)
+    for(std::vector<int>::size_type i = 0; i < nums.size(); i++)
     {
         count[nums[i]]++;
     }
-    for(int j = 0; j < nums.size(); j++)
+    for(std::vector<int>::size_type j = 0; j < nums.size(); j++)
     {
         if(count[nums[j]] == 1)
         {
             return j;
         }
     }
-    return -1;
+    return nums.size();
 }
 
-void printResult(const std::vector<int>& nums, int result)
+void printResult(const std::vector<int>& nums, std::vector<int>::size_type result)
 {
     if (nums.size() == 0)
     {
         std::cout << "源数据" << "{}" << std::endl;
+        std::cout << "结果: 没有唯一元素" << std::endl;
     }
     else
     {
         std::cout << "源数据" << "{";
-        for(int i = 0; i < nums.size() - 1; i++)
+        for(std::vector<int>::size_type i = 0; i < nums.size() - 1; i++)
         {
-            std::cout << nums[i] << ","; 
+            std::cout << nums[i] << ",";
         }
         std::cout << nums.back() << "}" << std::endl;
-        std::cout << "结果: 唯一元素索引为 " << result << std::endl;
+        if (result == nums.size())
+        {
+            std::cout << "结果: 没有唯一元素" << std::endl;
+        }
+        else
+        {
+            std::cout << "结果: 唯一元素索引为 " << result << std::endl;
+        }
     }
 }
